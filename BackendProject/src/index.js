9ffe74ace0263bf2fd21c94107fa024041ea1815
Sankar <sankarjyotichetia57@gmail.com>
2024-09-27 09:14:11 +0530

    configuring utils

--- BackendProject/src/index.js
+++ BackendProject/src/index.js
@@ -4,12 +4,21 @@
 // import { DB_NAME } from "./constants";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
+  path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port 8000: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed", err);
+  });
 
 /*
 import express from "express";
---
+++ BackendProject/src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+
+// with try and catch
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       message: err.message,
+//     });
+//   }
+// };
